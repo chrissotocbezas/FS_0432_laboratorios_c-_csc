04ddfbc5e11c5bc4455cd618e91eb407f01c30bc
chrissotocbezas <christopher.sotocabezas@ucr.ac.cr>
2026-05-25 10:40:54 -0600

    Agregar librerías al archivo de C++.

---
+++ metodos_numericos.cpp
@@ -0,0 +1,5 @@
+// Procedemos a agregar las librerías que vamos a utilizar para ejecutar nuestro archivo de C++-
+#include <iostream>
+#include <cmath>
+#include <vector> // Esta librería será necesaria porque trabajaremos con vectores y matrices.
+
