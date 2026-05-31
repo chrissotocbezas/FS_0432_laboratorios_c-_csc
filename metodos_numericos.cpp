b66cca3540a3ef77be263536ea68bde5af60fb5e
chrissotocbezas <christopher.sotocabezas@ucr.ac.cr>
2026-05-30 22:09:06 -0600

    Iniciar la función que contendrá las ecuaciones diferenciales para el sistema de Lorenz.

--- metodos_numericos.cpp
+++ metodos_numericos.cpp
@@ -3,3 +3,10 @@
 #include <cmath>
 #include <vector> // Esta librería será necesaria porque trabajaremos con vectores y matrices.
 
+// Procedemos a generar el lado derecho del Sistema de Lorentz.
+
+std::vector<double> lorenz(const std::vector<double>& r, double sigma, double rho, double beta) {
+    std::vector<double> F(3);
+
+    // A continuación colocamos las derivadas de la transformación de Lorentz.
+}
