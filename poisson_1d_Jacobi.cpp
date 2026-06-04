4c0f0a71c8b7000600096fc86b943ddd825c5a5b
chrissotocbezas <christopher.sotocabezas@ucr.ac.cr>
2026-06-03 22:09:24 -0600

    Agregar solución analítica y función fuente que se utilizarán en el Método de Jacobi. Se proceden a agregar los parámetros de la simulación como: número de divisiones, el paso, la tolerancia, el valor máximo de iteraciones a utilizar. En el siguiente paso, se tendrán las condiciones de frontera.

--- poisson_1d_Jacobi.cpp
+++ poisson_1d_Jacobi.cpp
@@ -6,3 +6,38 @@
 
 // Se procederá a cargar el archivo de matplotlibcpp.h del laboratorio 10, con el fin de que este sea ejecutable en el archivo del repositorio.
 
+namespace plt = matplotlibcpp; // Renombramos matplotlibcpp, con el fin de poder utilizar una notación estándar similar a la de Python.
+
+// En el método de Jacobi, tenemos que debemos mencionar la solución analítica del problema.
+// Podemos mencionar ahora esto con una función double, también debemos mencionar la función fuente, que se encuentra mencionada como f(x)
+
+// A continuación colocamos la solución analítica del problema.
+
+double solucion_analitica(double x) {
+    return -std::sin(M_PI * x) / (M_PI * M_PI); 
+}
+
+// Tenemos la solución analítica del problema, se multiplica pi dos veces, ya que se encuentra al cuadrado y en el caso del argumento de x, observamos que se encuentra multiplicando pi a x.
+
+// Recordar que debemos resolver el problema de forma numérica con una malla uniforme.
+
+// Colocamos la función fuente.
+
+double f(double x){
+    return std::sin(M_PI*x);
+}
+
+// A continuación, procedemos a definir la función principal, en donde procederemos a ejecutar las funciones anteriores y meter el método de Jacobi.
+// En esta función main, se encontrarán los parámetros a utilizar.
+
+int main() {
+    // Procedemos a colocar los parámetros de la simulación.
+
+    int N = 100; // Número de divisiones solicitadas.
+    double h = 1.0 / N; // Este valor corresponde al paso que se tiene en Jacobi. x = ih.
+    double tol = 1e-6; // Este valor corresponde a la tolerancia a utilizar para este método.
+    int max_iter = 1000000; // Aquí colocamos el número máxima de iteraciones.
+
+    // Procedemos a definir las condiciones de frontera que solicitamos.
+    
+}
