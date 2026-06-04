c2d2bb44d6da8afb56bc06e53d87a296658854a0
chrissotocbezas <christopher.sotocabezas@ucr.ac.cr>
2026-06-03 19:43:53 -0600

    Modificar ecuaciones del sistema de Lorenz por dimensión x-y-z.

--- metodos_numericos.cpp
+++ metodos_numericos.cpp
@@ -6,7 +6,18 @@
 // Procedemos a generar el lado derecho del Sistema de Lorentz.
 
 std::vector<double> lorenz(const std::vector<double>& r, double sigma, double rho, double beta) {
-    std::vector<double> F(3);
+    std::vector<double> drdt(3);
 
     // A continuación colocamos las derivadas de la transformación de Lorentz.
+    // Lo anterior corresponde a las ecuaciones diferenciales que debemos colocar.
+
+    drdt[0] = sigma * (r[1] - r[0]); // Esto corresponde a la ecuación diferencial de la posición en x.
+    drdt[1] = r[0] * (rho - r[2]) - r[1]; // Esto corresponde a la ecuación diferencial de la posición en y.
+    drdt[2] = r[0] * r[1] - beta * r[2]; // Esto corresponde a la ecuación diferencial de la posición en z.
+    
+    return drdt; // Al final, le pedimos que devuelva la posición en cada una de las variables.
+
+    /* Debemos tomar en cuenta, que para este código, el transformador de Lorentz, tiene la peculiaridad de tener tres ecuaciones diferenciales
+     que nos ayudan a observar el comportamiento que tiene cada componente el transformador de Lorenz. El hecho de que devolvamos drdt nada más,
+     esto nos ayuda a confirmarlo como un conjunto de arrays por dimensión. Estos arrays los usaremos en la siguiente parte del código.*/
 }
