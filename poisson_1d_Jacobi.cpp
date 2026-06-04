aa9966aab49181538d78841fa8c052dc72cb29c0
chrissotocbezas <christopher.sotocabezas@ucr.ac.cr>
2026-06-03 21:59:33 -0600

    Agregar librerías al archivo de poisson_1d_Jacobi.cpp

---
+++ poisson_1d_Jacobi.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <fstream>
+#include "matplotlibcpp.h" // Esta librería se descarga desde el repositorio de GitHub.FILE
+
+// Se procederá a cargar el archivo de matplotlibcpp.h del laboratorio 10, con el fin de que este sea ejecutable en el archivo del repositorio.
+
