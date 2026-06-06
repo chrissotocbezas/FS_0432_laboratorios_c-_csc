2c9bb92700be81abac2b6e56cdb27c955e0a57bf
chrissotocbezas <christopher.sotocabezas@ucr.ac.cr>
2026-06-05 23:05:19 -0600

    Agregar librerías necesarias para realizar gráficas en Python

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 # En este archivo se van a utilizar las herramientas de los métodos numéricos implementados en la tarea 03.
 # Se debe enlazar este archivo con métodos_numericos.cpp
+
+# Lo que hacemos ahora es importar las librerías de Python que debemos utilizar
+
+import numpy as np
+import matplotlib.pyplot as plt
+import metodos_numericos
