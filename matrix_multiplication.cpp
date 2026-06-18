9a2b6f3602a28cac1e10baef60079bcc05c7a569
chrissotocbezas <christopher.sotocabezas@ucr.ac.cr>
2026-06-18 10:43:44 -0600

    Agregar vectores y tamaño de inicialización para las respectivas matrices.

--- matrix_multiplication.cpp
+++ matrix_multiplication.cpp
@@ -3,3 +3,19 @@
 #include <iomanip>
 
 // Colocamos las librerías a utilizar para la ejecución de este laboratorio.
+
+int main(){
+    // En esta función main, procedemos a ejecutar la multiplicación de matrices.
+    // Debemos declarar algunas variables, luego debemos ejecutar la multiplicación de matrices con varios bucles for...
+
+    int N = 12800; // Este tamaño inicial lo procedemos a declarar.
+
+    // Procedemos a inicializar las matrices que las vamos a utilizar.
+
+    std::vector<double> A(N * N, 1.5);
+    std::vector<double> B(N * N, 2.0);
+    std::vector<double> C(N * N, 0.0);
+
+    // El vector C será el que contendrá toda la suma.
+
+}
