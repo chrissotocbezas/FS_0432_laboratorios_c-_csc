52699132da3c1c15f55b1d9d5ed9120ef50beca9
chrissotocbezas <christopher.sotocabezas@ucr.ac.cr>
2026-07-01 13:16:50 -0600

    Completar archivo .sh para correr en la terminal

--- pruebas_cpp.sh
+++ pruebas_cpp.sh
@@ -17,4 +17,25 @@ fi
 # Procedemos a inicializar con el encabezado CSV, esto con el objetivo de generar una tabla con los datos
 
 echo "procesos, tiempo" > $ARCHIVO_SALIDA
-echo "Creado el archivo
+echo "Creado el archivo $ARCHIVO_SALIDA con encabezados."
+
+# Procedemos a usar un bucle para ejecutar 1, 2 y 4 procesos, esto debido a que con ocho no corre por falta de CPU...
+
+for p in 1 2 4; do 
+    echo "Corriendo simulación con $p procesos..."
+
+    # Procedemos a ejecutar mpirun y extrae solo el número decimal flotante del tiempo
+
+    tiempos = $(mpirun -np $p $EJECUTABLE | grep -oP '[0-9]+\.[0-9]+')
+
+    if [ -z "$tiempo" ]; then
+        echo "Advertencia: No se pudo capturar el tiempo para $p procesos."
+    else
+        # Procedemos a guardar_ procesos, tiempo
+
+        echo "$p,$tiempo" >> $ARCHIVO_SALIDA
+        echo " -> ¡Completado! Tiempo registrado: $tiempo s"
+    fi
+done
+
+echo "Proceso finalizado. Datos guardados en '$ARCHIVO_SALIDA'."
