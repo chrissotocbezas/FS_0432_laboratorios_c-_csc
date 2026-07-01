5a6e5da0203cd1bb9c64c5230993027e87060c6e
chrissotocbezas <christopher.sotocabezas@ucr.ac.cr>
2026-07-01 13:56:54 -0600

    Agregar librerías para generar gráfica}

---
+++ graficacion_tiempos.py
@@ -0,0 +1,4 @@
+# Procedemos en este archivo a generar las gráficas necesarias para gráficar.
+import pandas as pd # Importar para tabular los datos de tiempo en el archivo .txt
+import matplotlib.pyplot as plt # Librería para generar gráficas.
+
